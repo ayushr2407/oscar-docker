1ef17e624de8d42ee27a408272f0189d5fdc8df4
Peter H-C <phc@srpc.ca>
2024-09-08 14:40:59 +0000

    CanadianVaccineCatalogueManager2.java typo

--- src/main/java/org/oscarehr/managers/CanadianVaccineCatalogueManager2.java
+++ src/main/java/org/oscarehr/managers/CanadianVaccineCatalogueManager2.java
@@ -72,7 +72,6 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
-import ca.uhn.fhir.rest.client.interceptor;
 import ca.uhn.fhir.rest.client.interceptor.AdditionalRequestHeadersInterceptor;
 import oscar.OscarProperties;
 import oscar.log.LogAction;
