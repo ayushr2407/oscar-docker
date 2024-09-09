81fa15b77b2b9b2abe081aa2846c7634b48603e4
Peter H-C <phc@srpc.ca>
2024-09-09 13:52:19 +0000

    CanadianVaccineCatalogueManager2.java missing imports

--- src/main/java/org/oscarehr/managers/CanadianVaccineCatalogueManager2.java
+++ src/main/java/org/oscarehr/managers/CanadianVaccineCatalogueManager2.java
@@ -33,14 +33,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import java.security.KeyStore;
 import javax.net.ssl.SSLContext;	
 import javax.net.ssl.HostnameVerifier;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClient;
+import org.apache.http.ssl.TrustStrategy;
 
 import org.apache.logging.log4j.Logger;
 import org.hl7.fhir.r4.model.Bundle;
